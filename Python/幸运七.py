10b7844ea925e980b0bd2eb01db42f7a00bb32ff
chi-yv <81824852+chi-yv@users.noreply.github.com>
2021-04-05 01:06:24 +0800

    更正骰子点数错误

--- Python/幸运七.py
+++ Python/幸运七.py
@@ -1,15 +1,15 @@
 from random import*
 for j in range(4,10):   # 从赢钱数额4~9中依次尝试
-    v = 0
+    v = 0    # 赢钱次数统计变量
     for i in range(10000):  # 10000次赌局
         m = 10  # 初始金钱
         while m > 0:
-            k = randint(0, 6) + randint(0, 6)   # 总点数
+            k = randint(1, 6) + randint(1, 6)   # 骰子总点数
             if (k == 7):
                 m += j
             else:
                 m -= 1
-            if (m >= 20):   # 胜利则退出
+            if (m >= 20):   # 赢钱则退出
                 v += 1
                 break
     if(v/10000>0.5):    # 计算胜率
