90669f1271f641e176dcc8bc19b27d96f1762321
chi-yv <81824852+chi-yv@users.noreply.github.com>
2021-04-04 13:43:46 +0800

    Update and rename 幸运七 to 幸运七.py

---
+++ Python代码/幸运七.py
@@ -0,0 +1,17 @@
+from random import*
+for j in range(4,10):   # 从赢钱数额4~9中依次尝试
+    v = 0
+    for i in range(10000):  # 10000次赌局
+        m = 10  # 初始金钱
+        while m > 0:
+            k = randint(0, 6) + randint(0, 6)   # 总点数
+            if (k == 7):
+                m += j
+            else:
+                m -= 1
+            if (m >= 20):   # 胜利则退出
+                v += 1
+                break
+    if(v/10000>0.5):    # 计算胜率
+        print('用户赢钱额度为{}时，保证总体赢钱'.format(j))
+        break
